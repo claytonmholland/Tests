5bf8d5a439fa39748af5effdb2c8a91fb2e682cd
claytonmholland <134184717+claytonmholland@users.noreply.github.com>
2023-05-21 00:01:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Select all videos with data attribute wb-embed="video"
+const videos = document.querySelectorAll('[wb-embed="video"]');
+
+// Loop through all the videos
+videos.forEach((video) => {
+  // Pause on initial load
+  video.pause();
+
+  video.addEventListener("click", (event) => {
+    if (video.paused) {
+      video.muted = false;
+      video.play();
+    } else {
+      video.pause();
+      //video.currentTime = 0; // resets video back to start on click
+    }
+  });
+}); // Create the observer for checking if video is in view
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.play();
+      } else {
+        entry.target.pause();
+      }
+    });
+  },
+  {
+    threshold: 0.2 // 0 (default) - element must be totally offscreen, 0.5 - 50%, 1 - element fully on screen
+  }
+);
+
+// Select all videos with data attribute wb-embed="video"
+const videos = document.querySelectorAll('[wb-embed="video"]');
+
+// Loop through all the videos
+videos.forEach((video) => {
+  // Pause on initial load
+  video.pause();
+  // Watch for entering viewport
+  observer.observe(video);
+});
